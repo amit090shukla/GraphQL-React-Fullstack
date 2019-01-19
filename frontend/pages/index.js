197544742e96d8a6ed5e1c229de2a8c7e8efdcb3
amit090shukla <amit090shukla@gmail.com>
2019-01-19 19:07:14 +0530

    Folder Seperation Backend + Frontend

---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Link from "next/link";
+
+const Home = () => (
+  <div>
+    <div>Hi Bello</div>
+  </div>
+);
+export default Home;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Link from "next/link";
+
+const Sell = () => (
+  <div>
+    <p>Sell Page</p>
+  </div>
+);
+export default Sell;
